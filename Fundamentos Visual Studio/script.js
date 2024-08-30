0624ab37e4e6248b335d3f6f11458ce53aea2242
igorjosecode <RES0091174@aluno.firjansenaisesi.com.br>
2024-08-29 21:34:14 -0300

    Add files via upload

---
+++ Fundamentos Visual Studio/script.js
@@ -0,0 +1,7 @@
+console.log("Olá, Mundo!")
+//let preco = prompt("Digite um preco")
+//document . write (preco);
+
+let nome = prompt ("Digite seu nome");
+document.write ("olá",nome,". Seja bem- vindo) ao site!");
+// olá, fulana. Seja bem vinda ao site! 
